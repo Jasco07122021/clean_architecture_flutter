63d22b6a04adaa6ffcac809fd79398f56d7274b6
Jasurbek Mominjonov <j.mominjonov@tengebank.uz>
2022-11-21 09:59:12 +0500

    Initial commit

---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Native Splash
+// EasyLocalization
+// Check Auth
+// ScreenUtil
+// Routing
+// Flavors
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
